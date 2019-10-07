b779d5afc4118a3a3d1b1bf5887da180bd433ea2
Adam Wolfe Gordon <awg@digitalocean.com>
2019-10-07 16:54:40 -0600

    Add a GitHub Action for generating previews from PRs

---
+++ github-actions/preview-dashboards/run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Get the base branch for the PR, so we can determine which files are changed.
+base_branch=$(jq -r '.pull_request.base.ref' "${GITHUB_EVENT_PATH}")
+
+# Determine which dashboards are affected by this change.
+files=$(git log --format='' --name-status "origin/${base_branch}".. -- '**/*.jsonnet' | grep -v '^D' | awk '{print $2}')
+libfiles=$(git log --format='' --name-status "origin/${base_branch}".. -- '**/*.libsonnet' | awk '{print $2}')
+dependencyFiles=()
+for l in "${libfiles[@]}"; do
+    dependencies=$(grep -R --include='*.jsonnet' -l -F "import '${l}'" . || true)
+    dependencyFiles+=("${dependencies}")
+done
+for d in "${dependencyFiles[@]}"; do
+    found=''
+    for f in "${files[@]}"; do
+        if [[ "${d}" == "./${f}" ]]; then
+            found='true'
+            break
+        fi
+    done
+
+    if [[ "${found}" == 'false' ]]; then
+        dd=$(echo "${d}" | sed -e 's/^..//')
+        files+=("${dd}")
+    fi
+done
+
+function generate_previews() {
+    export GRAFANA="${1}"
+    export NO_DOCKER='true'
+    for f in "${files[@]}"; do
+        folder=$(dirname "$f" | sed 's/^dashboards\///')
+        dbname=$(basename "$f" | sed 's/\.jsonnet$//')
+
+        url=$(scripts/preview.sh "${f}")
+        echo "${folder}/${dbname}::${url}"
+    done
+}
+
+urls=''
+links=''
+for preview in $(generate_previews "${1}"); do
+    dbname=$(echo "${preview}" | awk -F:: '{print $1}')
+    url=$(echo "${preview}" | awk -F:: '{print $2}')
+    urls="${url},${urls}"
+    links="[Preview for dashboard ${dbname}](${url})<br>${links}"
+done
+
+echo "::set-output name=links::${links}"
+echo "::set-output name=urls::${urls}"
