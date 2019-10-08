60094e63e1f41c9c47e8b2291d3a990fd0923930
Adam Wolfe Gordon <awg@digitalocean.com>
2019-10-07 20:19:52 -0600

    Add a GitHub Action for publishing dashboards on merge

---
+++ github-actions/publish-dashboards/run.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -euo pipefail
+
+GRAFANA="${1}"
+
+function get_folder_id() {
+    fname="\"${1}\""
+    existing=$(curl -X GET \
+                    -H "Authorization: Bearer ${TOKEN}" \
+                    -H 'Content-type: application/json' \
+                    -H 'Accept: application/json' \
+                    "${GRAFANA}/api/folders" | \
+                   jq ".[] | select(.title == ${fname}) | .id")
+    if [[ -n "${existing}" ]]; then
+        echo "${existing}"
+        return 0
+    fi
+
+    new=$(curl -X POST \
+               -H "Authorization: Bearer ${TOKEN}" \
+               -H 'Content-type: application/json' \
+               -H 'Accept: application/json' \
+               "${GRAFANA}/api/folders" \
+               --data-binary "{ \"title\": ${fname} }" | \
+              jq '.id')
+    echo "${new}"
+    return 0
+}
+
+bad=0
+for f in dashboards/**/*.jsonnet ; do
+    folder=$(dirname "$f" | sed 's/^dashboards\///')
+    folder_id=$(get_folder_id "${folder}")
+    dbname=$(basename "$f" | sed 's/\.jsonnet$//')
+
+    echo "Updating dashboard ${folder}/${dbname}"
+
+    if ! jsonnet -J /grafonnet-lib -J . "${f}" | \
+            jq "{ \"dashboard\": ., \"folderId\": ${folder_id}, \"overwrite\": true }" > \
+               "/tmp/${dbname}.json" ; then
+
+        echo "Failed to build ${folder}/${dbname}"
+        bad=$((bad+1))
+        continue
+    fi
+
+    if ! curl -f -X POST \
+         -H "Authorization: Bearer ${TOKEN}" \
+         -H 'Content-type: application/json' \
+         -H 'Accept: application/json' \
+         "${GRAFANA}/api/dashboards/db" \
+         --data-binary "@/tmp/${dbname}.json" ; then
+
+         echo "Failed to update ${folder}/${dbname}"
+         bad=$((bad+1))
+    fi
+done
+
+exit ${bad}
